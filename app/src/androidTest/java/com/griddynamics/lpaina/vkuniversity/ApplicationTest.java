1b8fdcc9bc28636b961c151a956dc4535035e60a
Liubov Paina <lubashka.994@mail.ru>
2015-12-10 02:45:06 +0300

    Init commit - add authorise from VK

---
+++ app/src/androidTest/java/com/griddynamics/lpaina/vkuniversity/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.griddynamics.lpaina.vkuniversity;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ getfriends/src/main/java/com/griddynamics/lpaina/getfriends/MainActivity.java
@@ -0,0 +1,41 @@
+package com.griddynamics.lpaina.getfriends;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKCallback;
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKError;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Context applicationContext = getApplicationContext();
+        VKSdk.initialize(applicationContext);
+        setContentView(R.layout.activity_main);
+        VKSdk.login(this);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (!VKSdk.onActivityResult(requestCode, resultCode, data, new VKCallback<VKAccessToken>() {
+            @Override
+            public void onResult(VKAccessToken res) {
+                Log.i("VK", "success");
+// Пользователь успешно авторизовался
+            }
+            @Override
+            public void onError(VKError error) {
+                Log.e("VK", "error");
+// Произошла ошибка авторизации (например, пользователь запретил авторизацию)
+            }
+        })) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
